85aabe96ec46608262f15c778d996281e7ba06d8
Alexandre Justino <alexandretok@gmail.com>
2017-03-07 14:40:53 -0300

    Agora foi

--- aprendendo.m
+++ aprendendo.m
@@ -2,38 +2,16 @@ entradas = [-1 -1 -1 -1; 0 1 0 1; 0 0 1 1];
 desejado = [-1; -1; -1; 1];
 pesos = [0.1 0.1 0.1];
 
-erroQuadratico = 1;
+erroQuadratico = 999; % vai conter o erro quadratico de cada iteracao
 
-erro = [];
+erro = []; % vetor que vai conter os erros para cada entrada
 
-n = 0.1;
+n = 0.01; % passo escolhido
 
-total = 0;
-limite = 10;
+totalDeIteracoes = 0; % calcular quantas iteracoes foram necessarias
+limiteIteracoes = 10; % limitar a um numero maximo de iteracoes
 
-while(erroQuadratico > 0.5 && limite > 0)
-  limite = limite - 1;
-  total = total + 1;
-	iteracao = 1;
-	for entrada = entradas
-		a = pesos*entrada
-		erro(iteracao, 1) = desejado(iteracao, 1) - ativacao(a)
-    % atualiza os pesos
-    % sprintf('pesos:')
-    % disp(pesos)
-    % sprintf('entrada:')
-    % disp(entrada)
-    pesos = pesos + n * transpose(entrada) * erro(iteracao, 1)
-		iteracao = iteracao + 1;
-	end
-	erroQuadratico = sum(power(erro, 2));
-  erro = [];
-  sprintf('erro quadratico: ')
-  disp(erroQuadratico)
-end
-
-total
-
+% funcao degrau (funcao de ativacao)
 function resultado = ativacao(x)
 	if(x<0)
 		resultado = -1;
@@ -41,3 +19,24 @@ function resultado = ativacao(x)
 		resultado = 1;
     end
 end
+
+while(erroQuadratico > 0.5 && limiteIteracoes > 0)
+
+  limiteIteracoes = limiteIteracoes - 1;
+  totalDeIteracoes = totalDeIteracoes + 1;
+	indexEntrada = 1;
+
+	for entrada = entradas
+		a = pesos * entrada; % calcula a saida
+		erro(indexEntrada, 1) = desejado(indexEntrada, 1) - ativacao(a); % calculo do erro
+    pesos = pesos + n * transpose(entrada) * erro(indexEntrada, 1); % atualiza os pesos
+		indexEntrada = indexEntrada + 1;
+	end
+	erroQuadratico = sum(power(erro, 2)) / (indexEntrada - 1); % calcula o erro quadratico
+  erro = [];
+  erroQuadratico
+end
+
+pesos
+totalDeIteracoes
+
