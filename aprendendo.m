c0df09effaf852819927824c875e6f30df830982
Alexandre Justino <alexandretok@gmail.com>
2017-03-07 14:25:16 -0300

    Acho que deu certo

--- aprendendo.m
+++ aprendendo.m
@@ -1,21 +1,38 @@
 entradas = [-1 -1 -1 -1; 0 1 0 1; 0 0 1 1];
-desejado = [-1 -1 -1 1];
+desejado = [-1; -1; -1; 1];
 pesos = [0.1 0.1 0.1];
 
 erroQuadratico = 1;
 
+erro = [];
 
-while(erroQuadratico > 0.5)
+n = 0.1;
+
+total = 0;
+limite = 10;
+
+while(erroQuadratico > 0.5 && limite > 0)
+  limite = limite - 1;
+  total = total + 1;
 	iteracao = 1;
 	for entrada = entradas
-		a = pesos*entrada;
-		a = a(:,1)
-		desejado(iteracao,:)
-		erro(iteracao) = desejado(:, iteracao) - a
+		a = pesos*entrada
+		erro(iteracao, 1) = desejado(iteracao, 1) - ativacao(a)
+    % atualiza os pesos
+    % sprintf('pesos:')
+    % disp(pesos)
+    % sprintf('entrada:')
+    % disp(entrada)
+    pesos = pesos + n * transpose(entrada) * erro(iteracao, 1)
 		iteracao = iteracao + 1;
 	end
-	erroQuadratico = 0.1;
+	erroQuadratico = sum(power(erro, 2));
+  erro = [];
+  sprintf('erro quadratico: ')
+  disp(erroQuadratico)
 end
+
+total
 
 function resultado = ativacao(x)
 	if(x<0)
