54b34d316e57b3da44bf80cef362bf26b584b7a8
Alexandre Justino <alexandretok@gmail.com>
2017-03-18 21:29:32 -0300

    Adiciona toolbox iRobot

---
+++ mlp.m
@@ -0,0 +1,52 @@
+entradas = [-1 -1 -1 -1; 0 1 0 1; 0 0 1 1];
+desejado = [-1; -1; -1; 1];
+pesos = [0.1 0.1 0.1];
+
+erroQuadratico = 999; % vai conter o erro quadratico de cada iteracao
+
+erro = []; % vetor que vai conter os erros para cada entrada
+
+n = 0.1; % passo escolhido
+
+totalDeIteracoes = 0; % calcular quantas iteracoes foram necessarias
+limiteIteracoes = 10; % limitar a um numero maximo de iteracoes
+
+% funcao degrau (funcao de ativacao)
+function resultado = ativacao(x)
+	if(x<0)
+		resultado = -1;
+	else
+		resultado = 1;
+    end
+end
+
+while(erroQuadratico > 0.5 && limiteIteracoes > 0)
+
+  limiteIteracoes = limiteIteracoes - 1;
+  totalDeIteracoes = totalDeIteracoes + 1;
+	indexEntrada = 1;
+
+	for entrada = entradas
+		a = pesos * entrada; % calcula a saida
+		erro(indexEntrada, 1) = desejado(indexEntrada, 1) - ativacao(a); % calculo do erro
+    pesos = pesos + n * transpose(entrada) * erro(indexEntrada, 1); % atualiza os pesos
+		indexEntrada = indexEntrada + 1;
+	end
+	erroQuadratico = sum(power(erro, 2)) / (indexEntrada - 1); % calcula o erro quadratico
+  erro = [];
+end
+
+pesos
+totalDeIteracoes
+erroQuadratico
+
+entrada = [];
+entrada(1,1) = -1;
+entrada(2,1) = input('x1:');
+entrada(3,1) = input('x2:');
+
+if(ativacao(pesos * entrada) == 1)
+  saida = 'true'
+else
+  saida = 'false'
+end
